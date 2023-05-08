01c8bf2a912bdf5e7cc578920f060599bb475c2f
Binghong-Leo-Li <97146983+Binghong-Leo-Li@users.noreply.github.com>
2023-05-07 19:05:31 -0700

    done..

--- scripts2.js
+++ scripts2.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const imageButtons = document.querySelectorAll(".imageButton");
     imageButtons.forEach((imageButton) => {
         imageButton.addEventListener("click", () => {
-            window.location.href = "index.html";
+            window.location.href = "index1.html";
         });
     });
 });
